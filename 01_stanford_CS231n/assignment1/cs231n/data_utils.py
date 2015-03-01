47f638748d53989d698e9f473b53deda08fa8c05
Liang Bo Wang <ccwang002@gmail.com>
2015-03-01 19:46:19 +0800

    fix read in data batches

--- 01_stanford_CS231n/assignment1/cs231n/data_utils.py
+++ 01_stanford_CS231n/assignment1/cs231n/data_utils.py
@@ -18,9 +18,9 @@ def load_CIFAR10(ROOT):
     ys = []
     for b in range(1, 6):
         f = os.path.join(ROOT, 'data_batch_%d' % (b, ))
-    X, Y = load_CIFAR_batch(f)
-    xs.append(X)
-    ys.append(Y)
+        X, Y = load_CIFAR_batch(f)
+        xs.append(X)
+        ys.append(Y)
     Xtr = np.concatenate(xs)
     Ytr = np.concatenate(ys)
     del X, Y
